06db5d339e91789e4ed3faff4328e4f9b0dc982c
Mykola Saliuk <haker0502@ukr.net>
2023-02-07 00:12:51 +0200

    MovieCard

---
+++ src/components/MovieCard/MovieCard.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const CardWrapper = styled.div`
+  margin-bottom: 10px;
+  display: flex;
+  gap: 5px;
+
+  /* align-items: center; */
+  /* padding: 5px 0; */
+`;
+
+export const CardImg = styled.img`
+  width: 200px;
+  height: auto;
+  /* display: flex; */
+  /* align-items: center; */
+  /* padding: 5px 0; */
+`;
+
+export const CardInfo = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+
+  /* align-items: center; */
+  /* padding: 5px 0; */
+`;
